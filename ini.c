5b64feee60f92b8805ea153a0f13c8fe6b8fe7b2
Will Eccles <WillEccles@users.noreply.github.com>
2020-01-06 13:12:02 -0500

    Initial commit

---
+++ ini.c
@@ -0,0 +1,444 @@
+#include "ini.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static const char* fmt_spaces = " %256[^=; ] = %256[^\n] ";
+static const char* fmt_no_spaces = " %256[^=; ]=%256[^ \n] ";
+
+static void freepair_r(struct inipair* root);
+static void freesec_r(struct inisection* sec);
+
+struct inisection* makesection(char* name) {
+    if (name == NULL) {
+        return NULL;
+    }
+    struct inisection* s = malloc(sizeof(struct inisection));
+    if (s == NULL) {
+        perror("makesection: malloc");
+        return NULL;
+    }
+    s->name = strdup(name);
+    s->head = NULL;
+    return s;
+}
+
+struct inifile* makeini(int flags) {
+    struct inifile* f = malloc(sizeof(struct inipair));
+    if (f == NULL) {
+        perror("makeini: malloc");
+        return NULL;
+    }
+    f->head = NULL;
+    f->default_section = malloc(sizeof(struct inisection));
+    if (f->default_section == NULL) {
+        perror("makeini: malloc");
+        free(f);
+        return NULL;
+    }
+    f->default_section->name = NULL;
+    f->default_section->head = NULL;
+    f->flags = flags;
+    return f;
+}
+
+struct inisection* freesection(struct inisection* sec) {
+    if (sec != NULL) {
+        freepair_r(sec->head);
+        struct inisection* next = sec->next;
+        // names are created with strdup
+        free(sec->name);
+        free(sec);
+        return next;
+    }
+    return NULL;
+}
+
+struct inipair* makepair(char* key, char* val) {
+    if (key == NULL) {
+        return NULL;
+    }
+
+    struct inipair* p = malloc(sizeof(struct inipair));
+    if (p == NULL) {
+        perror("makepair: malloc");
+        return NULL;
+    }
+    p->key = strdup(key);
+    p->val = val == NULL ? NULL : strdup(val);
+    return p;
+}
+
+struct inipair* freepair(struct inipair* pair) {
+    if (pair != NULL) {
+        struct inipair* next = pair->next;
+        // keys/vals are created with strdup
+        free(pair->key);
+        free(pair->val);
+        free(pair);
+        return next;
+    }
+    return NULL;
+}
+
+void freepair_r(struct inipair* root) {
+    if (root == NULL) {
+        return;
+    }
+    
+    while ((root = freepair(root)));
+}
+
+void freesec_r(struct inisection* sec) {
+    if (sec == NULL) {
+        return;
+    }
+
+    while ((sec = freesection(sec)));
+}
+
+void freeini(struct inifile* ini) {
+    if (ini == NULL) {
+        return;
+    }
+
+    freesec_r(ini->default_section);
+    freesec_r(ini->head);
+    free(ini);
+}
+
+struct inisection* section_insert(struct inifile* file, struct inisection* sec) {
+    if (file == NULL || sec == NULL) {
+        return NULL;
+    }
+
+    if (file->head == NULL) {
+        file->head = sec;
+        sec->next = NULL;
+        return sec;
+    } else {
+        struct inisection* prev = NULL;
+        struct inisection* curr = file->head;
+        int s;
+        while (curr->next != NULL) {
+            s = strcmp(sec->name, curr->name);
+            if (s < 0) { // less than
+                if (prev == NULL) {
+                    sec->next = file->head;
+                    file->head = sec;
+                    return sec;
+                } else {
+                    prev->next = sec;
+                    sec->next = curr;
+                    return sec;
+                }
+            } else if (s == 0) { // equal to
+                return curr;
+            }
+
+            prev = curr;
+            curr = curr->next;
+        }
+
+        curr->next = sec;
+        sec->next = NULL;
+        return sec;
+    }
+}
+
+struct inipair* pair_insert(struct inisection* sec, struct inipair* pair) {
+    if (sec == NULL || pair == NULL) {
+        return NULL;
+    }
+
+    if (sec->head == NULL) {
+        sec->head = pair;
+        sec->head->next = NULL;
+        return sec->head;
+    } else {
+        struct inipair* prev = NULL;
+        struct inipair* curr = sec->head;
+        int s;
+        while (curr->next != NULL) {
+            s = strcmp(pair->key, curr->key);
+            if (s < 0) { // less than (insert before)
+                if (prev == NULL) {
+                    sec->head = pair;
+                    pair->next = curr;
+                    return sec->head;
+                } else {
+                    prev->next = pair;
+                    pair->next = curr;
+                    return pair;
+                }
+            } else if (s == 0) { // equal to (overwrite)
+                if (prev == NULL) {
+                    sec->head = pair;
+                    pair->next = curr->next;
+                } else {
+                    prev->next = pair;
+                    pair->next = curr->next;
+                }
+                freepair(curr);
+                return pair;
+            }
+
+            prev = curr;
+            curr = curr->next;
+        }
+
+        curr->next = pair;
+        pair->next = NULL;
+        return pair;
+    }
+}
+
+int loadinifromfile(struct inifile* inif, char* filename) {
+    if (inif == NULL || filename == NULL || inif->default_section == NULL) {
+        return 1;
+    }
+
+    FILE* infile = fopen(filename, "r");
+    if (NULL == infile) {
+        perror("loadinifromfile: fopen");
+        return 1;
+    }
+
+    const char* keyvalfmt;
+    if (inif->flags & INIO_SPACE_AROUND_DELIM) {
+        keyvalfmt = fmt_spaces;
+    } else {
+        keyvalfmt = fmt_no_spaces;
+    }
+    
+    char tmpline[512] = {0};
+    char tmpkey[256] = {0};
+    char tmpval[256] = {0};
+    char tmpsection[256] = {0};
+
+    // default to inserting to the default section
+    struct inisection* tmpsec = inif->default_section;
+
+    int rval = 0;
+    while (!feof(infile) && !ferror(infile)) {
+        if (tmpline != fgets(tmpline, 512, infile)) {
+            continue;
+        }
+
+        if (1 == (rval = sscanf(tmpline, " [%256[^]]]", tmpsection))) {
+            // set the current section
+            tmpsec = makesection(tmpsection);
+            struct inisection* tmpsec2 = section_insert(inif, tmpsec);
+            if (tmpsec2 != tmpsec) {
+                free(tmpsec);
+                tmpsec = tmpsec2;
+            }
+            continue;
+        }
+
+        // check for both a key and a value
+        if (2 == (rval = sscanf(tmpline, keyvalfmt, tmpkey, tmpval))) {
+            // insert the new key/value pair into the current section
+            pair_insert(tmpsec, makepair(tmpkey, tmpval));
+            continue;
+        }
+
+        // check for a key with no value
+        if (inif->flags & INIO_ALLOW_EMPTY) {
+            if (1 == (rval = sscanf(tmpline, " %256[^=; ] \n", tmpkey))) {
+                pair_insert(tmpsec, makepair(tmpkey, NULL));
+                continue;
+            }
+        }
+    }
+    
+    fclose(infile);
+
+    return 0;
+}
+
+struct inifile* newinifromfile(char* filename, int flags) {
+    if (filename == NULL) {
+        return NULL;
+    }
+
+    struct inifile* inif = makeini(flags);
+    if (inif == NULL) {
+        return NULL;
+    }
+
+    if (1 == loadinifromfile(inif, filename)) {
+        freeini(inif);
+        return NULL;
+    }
+
+    return inif;
+}
+
+void parseini(struct inifile* ini, ini_pair_op cb) {
+    ini_foreach(ini, cb);
+}
+
+void ini_foreach(struct inifile* ini, ini_pair_op cb) {
+    for (struct inipair* p = ini->default_section->head; p; p = p->next) {
+        cb(ini->default_section, p);
+    }
+
+    for (struct inisection* s = ini->head; s; s = s->next) {
+        for (struct inipair* p = s->head; p; p = p->next) {
+            cb(s, p);
+        }
+    }
+}
+
+struct inisection* ini_getsection(struct inifile* ini, char* name) {
+    if (ini == NULL) {
+        return NULL;
+    }
+
+    if (name == NULL) {
+        return ini->default_section;
+    }
+
+    for (struct inisection* s = ini->head; s; s = s->next) {
+        if (0 == strcmp(name, s->name)) {
+            return s;
+        }
+    }
+
+    return NULL;
+}
+
+struct inipair* inisection_getpair(struct inisection* section, char* key) {
+    if (section == NULL || key == NULL) {
+        return NULL;
+    }
+
+    for (struct inipair* p = section->head; p; p = p->next) {
+        if (0 == strcmp(key, p->key)) {
+            return p;
+        }
+    }
+    
+    return NULL;
+}
+
+struct inipair* ini_getpair(struct inifile* ini, char* section, char* key) {
+    struct inisection* s = ini_getsection(ini, section);
+    if (s == NULL) {
+        return NULL;
+    }
+
+    return inisection_getpair(s, key);
+}
+
+int writeinitofile(struct inifile* ini, char* filename) {
+    if (ini == NULL || filename == NULL) {
+        return 1;
+    }
+
+    FILE* outfile = fopen(filename, "w");
+    if (outfile == NULL) {
+        perror("writeinitofile: fopen");
+        return 1;
+    }
+
+    for (struct inipair* p = ini->default_section->head; p; p = p->next) {
+        if (p->val != NULL) {
+            fprintf(outfile, "%s=%s\n", p->key, p->val);
+        } else {
+            if (ini->flags & INIO_ALLOW_EMPTY) {
+                fprintf(outfile, "%s=\n", p->key);
+            }
+        }
+    }
+
+    fprintf(outfile, "\n");
+
+    for (struct inisection* s = ini->head; s; s = s->next) {
+        if (s->head == NULL) {
+            continue;
+        } else {
+            fprintf(outfile, "[%s]\n", s->name);
+        }
+
+        for (struct inipair* p = s->head; p; p = p->next) {
+            if (p->val != NULL) {
+                fprintf(outfile, "%s=%s\n", p->key, p->val);
+            } else {
+                if (ini->flags & INIO_ALLOW_EMPTY) {
+                    fprintf(outfile, "%s=\n", p->key);
+                }
+            }
+        }
+
+        fprintf(outfile, "\n");
+    }
+
+    fclose(outfile);
+
+    return 0;
+}
+
+char* pair_setval(struct inipair* pair, char* val) {
+    if (pair == NULL) {
+        return NULL;
+    }
+
+    if (pair->val != NULL) {
+        free(pair->val);
+        pair->val = NULL;
+    }
+
+    if (val != NULL) {
+        pair->val = strdup(val);
+    }
+
+    return pair->val;
+}
+
+struct inipair* ini_put(struct inifile* ini, char* section, char* key, char* val) {
+    if (ini == NULL || key == NULL) {
+        return NULL;
+    }
+
+    struct inisection* s = ini_getsection(ini, section);
+    if (s == NULL) {
+        s = section_insert(ini, makesection(section));
+        if (s == NULL) {
+            return NULL;
+        }
+    }
+
+    struct inipair* p = inisection_getpair(s, key);
+    if (p == NULL) {
+        p = pair_insert(s, makepair(key, val));
+        if (p == NULL) {
+            return NULL;
+        }
+    }
+
+    return p;
+}
+
+struct inipair* ini_set(struct inifile* ini, char* section, char* key, char* val) {
+    if (ini == NULL || key == NULL) {
+        return NULL;
+    }
+
+    struct inisection* s = ini_getsection(ini, section);
+    if (s == NULL) {
+        return NULL;
+    }
+
+    struct inipair* p = inisection_getpair(s, key);
+    if (p == NULL) {
+        return NULL;
+    }
+
+    if (NULL == pair_setval(p, val)) {
+        return NULL;
+    }
+
+    return p;
+}
---
+++ ini.h
@@ -0,0 +1,208 @@
+#ifndef INI_H
+#define INI_H
+
+/*
+ * Options for INI files. By default, options are assumed off.
+ */
+enum INI_OPT {
+    INIO_NONE = 0, // no options specified
+    INIO_SPACE_AROUND_DELIM = 1 << 0, // allow spaces around delimiters, i.e. name = val rather than name=val
+    INIO_ALLOW_EMPTY = 1 << 1, // allow empty values for keys
+    INIO_ALL = 0xFFFFFFFF, // allow all options
+};
+
+/*
+ * Key-value pair in an INI file.
+ * Values MUST be set to dynamically-allocated strings!
+ * You should not write, only read. If you wish to set the
+ * value, use pair_setval() or one of the other value-setting functions.
+ */
+struct inipair {
+    struct inipair* next;
+    char* key;
+    char* val;
+};
+
+/*
+ * Section in an INI file.
+ */
+struct inisection {
+    char* name;
+    struct inipair* head;
+    struct inisection* next;
+};
+
+/*
+ * Structure representing an INI file.
+ * This is the only structure in this file you
+ * should care about, except for manually iterating
+ * through the INI file.
+ *
+ * You should initialize this with either makeini() or
+ * newinifromfile().
+ */
+struct inifile {
+    struct inisection* head; // head of the list of sections, kept in alphabetical
+    struct inisection* default_section; // default section (options found before the first section)
+    int flags; // flags determining parsing behavior (see enum INI_OPT)
+};
+
+/*
+ * Callback function used when you call parseini().
+ * The arguments are the INI section in which the pair was found
+ * and the pair itself. Note that the 'name' field of the default
+ * INI section (for options which appear before the first occurrence
+ * of a [section] in the file) is NULL.
+ */
+typedef void(*ini_pair_op)(struct inisection*, struct inipair*);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Make a new INI file structure.
+ * Returns NULL on error.
+ */
+extern struct inifile* makeini(int flags);
+
+/*
+ * Parse an INI file into a new inifile structure.
+ * Filename must not be NULL. Flags should be set from
+ * enum INI_OPT.
+ * Returns NULL on error, else returns a pointer to the
+ * parsed INI file structure.
+ */
+extern struct inifile* newinifromfile(char* filename, int flags);
+
+/*
+ * Load data into an existing inifile structure,
+ * presumably created by makeini() or newinifromfile().
+ * If inif contains values already, they will be kept.
+ * Duplicate values will be overwritten.
+ * Returns 0 on success, else 1.
+ */
+extern int loadinifromfile(struct inifile* inif, char* filename);
+
+/*
+ * Writes an INI file structures contents to the disk.
+ * Returns 0 on success, 1 on failure.
+ * If the file contains comments, they will be deleted, unless I update
+ * this utility.
+ */
+extern int writeinitofile(struct inifile* ini, char* filename);
+
+/*
+ * Parse an INI file. Every key-value pair is passed to the callback function,
+ * along with its section. This callback function will be called repeatedly
+ * until all pairs in the file have been parsed. This is effectively
+ * a wrapper around ini_foreach with a less general name.
+ */
+extern void parseini(struct inifile* ini, ini_pair_op cb);
+
+/*
+ * Loops through all sections and pairs in an INI file and performs
+ * the given operation on them. Note that the default section's
+ * 'name' field is NULL.
+ */
+extern void ini_foreach(struct inifile* ini, ini_pair_op cb);
+
+/*
+ * Returns the section in an INI file with the given name.
+ * If name is NULL, the default section is returned. If the
+ * name is not found, NULL is returned.
+ */
+extern struct inisection* ini_getsection(struct inifile* ini, char* name);
+
+/*
+ * Returns the key-value pair in a given section with a given key.
+ * If the key is not found, NULL is returned.
+ * If the section provided is NULL, it's assumed that the key is in
+ * the default section of the file.
+ * Any other error results in NULL being returned.
+ */
+extern struct inipair* inisection_getpair(struct inisection* section, char* key);
+
+/*
+ * Attempts to find a key-value pair in an INI file given a section name and
+ * key. If the section name is NULL, the default section is searched.
+ * This is a wrapper around ini_getsection() and inisection_getpair().
+ */
+extern struct inipair* ini_getpair(struct inifile* ini, char* section, char* key);
+
+/*
+ * Sets the value of a key-value pair. This is the only recommended way
+ * to set the value of a pair, as it deals with string duplication for you.
+ * Returns a pointer to the new value, or NULL if there was an error. NULL
+ * is also returned if the new value is NULL.
+ */
+extern char* pair_setval(struct inipair* pair, char* val);
+
+/*
+ * Sets the value of a given key in a given section. If the section and/or
+ * key is not found, they will be created. NULL section implies default section.
+ * Returns a pointer to the pair which contains the key, or NULL on error.
+ */
+extern struct inipair* ini_put(struct inifile* ini, char* section, char* key, char* val);
+
+/*
+ * Much like ini_put, except that if a section or key is not found, it will
+ * not be created for you.
+ */
+extern struct inipair* ini_set(struct inifile* ini, char* section, char* key, char* val);
+
+/*
+ * Frees an entire INI file structure.
+ */
+extern void freeini(struct inifile* ini);
+
+/*
+ * Make a new section. Returns NULL on error.
+ * Name is required.
+ * The name parameter is duplicated, so overwriting the original pointer
+ * will have no effect on the section's name.
+ */
+extern struct inisection* makesection(char* name);
+
+/*
+ * Frees a section and returns the next section, or NULL if there isn't one.
+ * This also frees all pairs in the section to avoid leaking memory.
+ */
+extern struct inisection* freesection(struct inisection* sec);
+
+/*
+ * Make a new pair. Returns NULL on error.
+ * Only key must be provided. If key is NULL, there will be an error.
+ * Both key and val are duplicated, so they may be reusable pointers.
+ */
+extern struct inipair* makepair(char* key, char* val);
+
+/*
+ * Frees a pair and returns a pointer to the next pair in the list,
+ * or returns NULL if there isn't one.
+ */
+extern struct inipair* freepair(struct inipair* pair);
+
+/*
+ * Insert a section into an INI file structure.
+ * If returned value is NULL, there was an error.
+ * If the returned value != sec, then sec should be freed and ignored, as the
+ * returned value is a pre-existing section with the same name.
+ * If successful, the return value == sec.
+ */
+extern struct inisection* section_insert(struct inifile* file, struct inisection* sec);
+
+/*
+ * Insert a pair into a section in an INI file.
+ * If the return value is NULL, there was an error.
+ * If the return value is non-NULL, it will be equal to pair.
+ * If the supplied value is alreaady found in the list, the existing one
+ * will be freed and the new one will replace it.
+ */
+extern struct inipair* pair_insert(struct inisection* sec, struct inipair* pair);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
